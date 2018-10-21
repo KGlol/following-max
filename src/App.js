eb917d6d465ca27ac76deab2cdc56616c478efb4
kg <micyounian@gmail.com>
2018-10-21 15:57:52 +0800

    disposision

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import Person from './Person/Person';
 import './App.css';
+// import Radium, { StyleRoot } from 'radium';
 
 class App extends Component {
       //组件的state
       state = {
         person: [
-          { name: 'kg', age: 12 },
-          { name: 'df', age: 111 },
-          { name: 'sdsd', age: 23 }
+          { id: 'id1', name: 'kg', age: 12 },
+          { id: 'id2', name: 'df', age: 111 },
+          { id: 'id3', name: 'sdsd', age: 23 }
         ],
         
         anotherState: 'something else',
@@ -20,9 +21,9 @@ class App extends Component {
         switchNumberHander = ( name, age ) => { //函数中的参数
           this.setState( {
             person : [
-              { name: name, age: age },
-              { name: 'dddd', age: 122 },
-              { name: 'sdsd', age: 13 }
+              { id: 'id1', name: name, age: age },
+              { id: 'id2', name: 'dddd', age: 122 },
+              { id: 'id3', name: 'sdsd', age: 13 }
             ]
             }
           )//
@@ -31,9 +32,9 @@ class App extends Component {
         nameChangeHander = (e) => {
           this.setState( {
             person : [
-              { name: 'kg', age: '10' },
-              { name: 'dddd', age: 122 },
-              { name: e.target.value, age: 13 }
+              { id: 'id1', name: 'kg', age: '10' },
+              { id: 'id2', name: 'dddd', age: 122 },
+              { id: 'id3', name: e.target.value, age: 13 }
             ]
             }
           )
@@ -53,44 +54,86 @@ class App extends Component {
 
         //删除person函数
         deletePersonHander = ( index ) => {
-          const person = this.state.person;
+          /**
+           * 数组是refence type，直接赋给别的变量，只是使变量指向原数组
+           * 成熟的习惯是复制一分变量，更改复制品。
+           * 方法一  使用数组的slice方法复制数组
+           * 方法二  使用展开运算符
+           */
+          //const person = this.state.person.slice(); 
+          const person = [...this.state.person];
           person.splice(index, 1);
           this.setState({person: person});
         }
         
   render() {
-    return (
-      <div className="App">
-        <h1>This is an h1</h1>
-         {this.state.person.map( (eachPerson, index) => {
-            return 
-              <Person 
-              name={eachPerson.name} 
-              age={eachPerson.age}
-              click={this.deletePersonHander.bind(this, index)} />//传参时要使用箭头函数
-          } )}
-          {/* {this.state.showPerson ?  
-          <div>
-            <Person 
-              name={this.state.person[1].name} 
-              age={this.state.person[1].age}
-              //传参的两种方式之一使用.bind(this, ...)（推荐）
-              click={this.switchNumberHander.bind(this, '!!!!!', 100)} 
-              change={this.nameChangeHander} />
-            <Person 
-              name={this.state.person[0].name} 
-              age={this.state.person[0].age}
-              change={this.nameChangeHander} />
-            <Person 
-              name={this.state.person[2].name} //
-              age={this.state.person[2].age}
-              change={this.nameChangeHander} >component Person's children.</Person> 
-          </div>  : null}  */}
 
+    const style = {
+      color: 'white',
+      backgroundColor: 'green',
+      border: '1px solid transparent',
+      boxShadow: '0 1px 4px black',
+      outline: 'none',
+      font: 'inherit',
+      cursor: 'pointer',
+      // ':hover': {
+        // backgroundColor: 'yellow',
+        // color: 'black'
+      // }
+    }
+
+    let person = null;
+
+    if ( this.state.showPerson ) {
+      person =  ( 
+      <div>
+      {this.state.person.map( (eachPerson, index) => {
+        //<div>套在这个位置就不行？？？
+       return <Person 
+       //return 后面要跟东西 直接换行会报错！！！
+            name={eachPerson.name} 
+            age={eachPerson.age}
+            click={() => this.deletePersonHander(index)} 
+            key={eachPerson.id} />
+          } )} 
+      </div>);
+
+      style.backgroundColor= 'grey';
+      // style[':hover'] = {
+      //   backgroundColor: 'salmon',
+      //   color: 'white'
+      // };
+    }
+
+    const classes = [];
+    if(this.state.person.length <= 2) classes.push('red');
+    if(this.state.person.length <= 1) classes.push('bold');
+    return (
+        <div className="App">
+          <h1 className={classes.join(' ')}>This is an h1</h1>
+          {person}
+            {/* {this.state.showPerson ?  
+            <div>
+              <Person 
+                name={this.state.person[1].name} 
+                age={this.state.person[1].age}
+                //传参的两种方式之一使用.bind(this, ...)（推荐）
+                click={this.switchNumberHander.bind(this, '!!!!!', 100)} 
+                change={this.nameChangeHander} />
+              <Person 
+                name={this.state.person[0].name} 
+                age={this.state.person[0].age}
+                change={this.nameChangeHander} />
+              <Person 
+                name={this.state.person[2].name} //
+                age={this.state.person[2].age}
+                change={this.nameChangeHander} >component Person's children.</Person> 
+            </div>  : null}  */}
+
+            
+          <button onClick={this.toggleShowHander} style={style}>switch name</button>
           
-        <button onClick={this.toggleShowHander}>switch name</button>
-        
-      </div>
+        </div>
     )
   }
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,13 +1,20 @@
 import React from 'react';
+// import Radium from 'radium';
+import './Person.css';
 
-const person = ( props ) => {
+const Person = ( props ) => {
+  // const style = {
+  //   '@media (min-width: 500px)': { width: '450px' },
+  //   // backgroundColor: 'red',
+  // 
+
   return (
-        <div>
-          <p onClick={props.click}>I'm {props.name}, and I'm {props.age} years old.</p>
-          <p>{props.children}</p>
-          <input type="text" onChange={props.change} ></input>
-        </div>
-        );
-}
+    <div className="person">
+      <p onClick={props.click}>I'm {props.name}, and I'm {props.age} years old.</p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.change} />
+    </div>
+  );
+  }
 
-export default person;
+export default Person;
